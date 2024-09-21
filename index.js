74557d340e6233632ef984a5fdf240199e782cf3
himanshuc11 <himanshuchhatpar@gmail.com>
2024-09-21 19:16:52 +0530

    feat: added network connection between express and mongo container

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://mongo-backend:27017";
+const client = new MongoClient(uri);
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Docker with live reload')
+app.get('/', async (req, res) => {
+  const database = client.db('test');
+  const movies = database.collection('movies');
+  const list = await movies.findOne()
+  res.send(JSON.stringify(list))
 })
 
 app.listen(port, () => {

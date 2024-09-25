d27fa8deeaa13d8c8ad3d3037331434006bfb375
himanshuc11 <himanshuchhatpar@gmail.com>
2024-09-25 10:25:26 +0530

    feat: added secrets in docker

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const { MongoClient } = require("mongodb");
 
+const fs = require('node:fs');
+fs.readFile('/run/secrets/express_env', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  process.env = null
+});
+
 const uri = "mongodb://mongo-backend:27017";
 const client = new MongoClient(uri);
 
 const app = express()
 const port = 3000
 
+console.log(process.env)
 app.get('/', async (req, res) => {
+
   const database = client.db('test');
   const movies = database.collection('movies');
   const list = await movies.findOne()
